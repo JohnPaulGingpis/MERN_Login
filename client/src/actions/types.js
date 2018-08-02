7d1e7d5000f907aa03eeff6e2c6cde0ab4c93d33
Hrag <hraghovsep@hotmail.com>
2018-08-01 19:21:38 -0700

    Deleted Unnecessary Files
    More Code Clean Up

--- client/src/actions/types.js
+++
@@ -1,7 +0,0 @@
-export const GET_USERS = 'GET_USERS';
-export const ADD_USER = 'ADD_USER';
-export const DELETE_USER = 'DELETE_USER';
-export const USERS_LOADING = 'USERS_LOADING';
-export const VERIFY_USERS = 'VERIFY_USERS';
-export const SIGNIN_USER = 'SIGNIN_USER';
-export const LOGOUT_USERS = 'LOGOUT_USERS';
